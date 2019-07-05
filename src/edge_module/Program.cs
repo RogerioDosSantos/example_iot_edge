ab6e5c697331c4f23df9f543dcea3d12d4342f49
Roger <roger.santos.main@gmail.com>
2019-07-05 15:06:19 -0500

    Created the mock project with Hello World

---
+++ src/edge_module/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace edge_module
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
